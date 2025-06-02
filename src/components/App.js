207c8b85aa72794c0a2eae6e75cbe5a89f6ab328
HERCULES0000 <anuragbachheti1999@gmail.com>
2025-06-02 17:42:35 +0530

    first commit on reviews

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,15 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+import Review from './Review';
 
+function App() {
+
+
+  return (
+    <div>
+       <h1 id="review-heading">Our Reviews</h1>
+        <Review/>
+    </div>
+  )
+}
+
+export default App
